1c5c16be8f56ff124022151c3c6569585ff8fbaa
oriolholguint <oriolholguint@gmail.com>
2021-11-26 13:16:58 +0100

    Añado metodo obtener preguntas para la partida y generador de numeros aleatorios

--- app/src/main/java/com/example/madu_project/MainActivity.java
+++ app/src/main/java/com/example/madu_project/MainActivity.java
@@ -61,4 +61,64 @@ public class MainActivity extends AppCompatActivity
 
     }
 
+    /**
+     * Se obtienen preguntas aleatorias de un array enviado por parametros.
+     * @param preguntas array donde obtener preguntas
+     * @param cantidadPreguntas cantidad de preguntas aleatorias a obtener
+     * @return array con preguntas aleatorias
+     */
+    public static ArrayList<Pregunta> getPreguntasPartida(Pregunta[] preguntas,int cantidadPreguntas)
+    {
+        ArrayList<Pregunta> preguntasSeleccionadas = new ArrayList<>(); //Preguntas que saldran en la partida
+        ArrayList<Integer> numerosAleatorios = new ArrayList<>(); //Numeros aleatorios para no repetirlos
+        int numeroAleatorio = 0;
+
+        for(int i = 0; i < cantidadPreguntas; i++) //Bucle con la cantidad de preguntas necesarias
+        {
+            boolean preguntaCorrecta = false;
+
+            while(!preguntaCorrecta) //Bucle para elegir pregunta que no se repita
+            {
+                numeroAleatorio = obtenerNumeroAleatorio(0, preguntas.length - 1);
+                int counter = 0;
+                boolean numeroRepetido = false;
+                //Si el num no esta repetido y aun quedan por revisar mas numeros entra aqui
+                while(!numeroRepetido && counter < numerosAleatorios.size())
+                {
+                    if(numeroAleatorio == numerosAleatorios.get(counter)) //Si el num aleatorio esta repetido
+                    {
+                        numeroRepetido = true;
+                    }
+                    else //Si el num aleatorio no esta repetido
+                    {
+                        counter++; //Itera el contador para comprobar siguiente numero
+                    }
+                }
+
+                if(!numeroRepetido)//Si el numero no esta repetido
+                {
+                    //Agrego el numero aleatorio al array de los numeros que ya han salido
+                    numerosAleatorios.add(numeroAleatorio);
+                    //Agrego la pregunta al array de preguntas selecciondas
+                    preguntasSeleccionadas.add(preguntas[numeroAleatorio]);
+                    preguntaCorrecta = true; //Señalo que la respuesta se agrega correctamente para salir del bucle
+                }
+            }
+        }
+
+        return preguntasSeleccionadas;
+    }
+
+    /**
+     * Devuelve un numero aleatorio dado un minimo y un maximo, ambos incluidos
+     * @param min numero minimo a obtener
+     * @param max numero maximo a obtener
+     * @return numero aleatorio
+     */
+    public static int obtenerNumeroAleatorio(int min, int max)
+    {
+        int range = (max -min) + 1;
+        return (int) (Math.random() * range + min);
+    }
+
 }
